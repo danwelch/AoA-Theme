03d543c1d5267ac45bf3015ca1e5304091de9df3
System Administrator <root@lt-mb11134.local>
2017-11-04 18:34:20 -0400

    setting up BS

---
+++ app/public/wp-content/themes/aoa/footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package aoa
+ */
+
+?>
+
+<footer>
+	Footer
+</footer><!-- #colophon -->
+<?php wp_enqueue_script( 'aoa-navigation', get_template_directory_uri() . '/js/navigation.js' );?>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ app/sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"blevy","$P$BQeBcX9SBqLks8ywmKgz8uK8o7dAX/0","blevy","blevy@launchthat.com","","2017-11-03 20:36:57","",0,"blevy");
