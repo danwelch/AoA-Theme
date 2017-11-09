9655d96ce4f691c7431421da47b957d1a3d1bf66
System Administrator <root@lt-mb11134.local>
2017-11-08 22:30:07 -0500

    stashing

--- app/sql/local.wp_term_relationships.sql
+++ app/sql/local.wp_term_relationships.sql
@@ -2,4 +2,9 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_term_relationships` VALUES
-(1,1,0);
+(1,1,0),
+(7,2,0),
+(24,2,0),
+(25,2,0),
+(26,2,0),
+(27,2,0);
--- app/sql/local.wp_usermeta.sql
+++ app/sql/local.wp_usermeta.sql
@@ -15,4 +15,10 @@ INSERT INTO `wp_usermeta` VALUES
 (11,1,"wp_capabilities","a:1:{s:13:\"administrator\";b:1;}"),
 (12,1,"wp_user_level","10"),
 (13,1,"dismissed_wp_pointers",""),
-(14,1,"show_welcome_panel","1");
+(14,1,"show_welcome_panel","1"),
+(15,1,"session_tokens","a:1:{s:64:\"0d83eb1b36dd239cc746ca2cf07c32e7fb9b7420bda152496a4b6dc3cc5782b1\";a:4:{s:10:\"expiration\";i:1509975975;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:120:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36\";s:5:\"login\";i:1509803175;}}"),
+(16,1,"wp_dashboard_quick_press_last_post_id","3"),
+(17,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
+(18,1,"managenav-menuscolumnshidden","a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}"),
+(19,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}"),
+(20,1,"nav_menu_recently_edited","2");
