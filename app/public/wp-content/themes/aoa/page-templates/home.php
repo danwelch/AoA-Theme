a0d5f7249501318f99ab8775ec1da7b161f95cf9
System Administrator <root@lt-mb11134.cfl.rr.com>
2017-12-03 13:21:03 -0500

    adding home.php file

---
+++ app/public/wp-content/themes/aoa/page-templates/home.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Template Name: Home
+ *
+ * Template for displaying Homepage.
+ *
+ * @package understrap
+ */
+
+get_header(); ?>
+
+<body <?php body_class(); ?>>
+	<main>
+		<div id="rotator"></div>
+		<!-- Home Page Hero -->
+		<nav class="nav nav--container">
+			<div class="nav--logo">
+				<img src="wp-content/themes/aoa/img/src/logo-nav.png" alt="Ability logo">
+			</div>
+			<ul class="nav nav--items">
+				<li class="nav--items-list"><a class="active" href="#">Home</a></li>
+				<li class="nav--items-list"><a href="#">Parent Resources</a></li>
+				<li class="nav--items-list"><a href="#">Curriculum</a></li>
+				<li class="nav--items-list"><a href="#">Yearbook</a></li>
+			</ul>
+		</nav>
+
+		<!-- Home Page Hero -->
+		<section class="hero">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="hero__logo">
+						<img src="wp-content/themes/aoa/img/src/logo.png" alt="Ability logo">
+					</div>
+				</div>	
+				<div class="row justify-content-center">
+					<h1 class="hero__title align-self-center">Specialized Learning <br/>Provided by People Who Care</h1>
+				</div>	
+				<div class="row justify-content-center">
+					<p class="hero__hummingbird">The Academy of Ability is a school for children with physical and learning disabilities. We provide individualized learning plans that challenge students to achieve their full academic and physical potential.</p>
+				</div>
+				<div class="row justify-content-center">
+					<button type="button" class="btn btn-primary">Support Special Education</button>
+					<button type="button" class="btn btn-secondary">Donate Now</button>
+				</div>
+			<div>
+		</section>
+
+		<!-- Hero With Subtitle -->
+		<section class="hero">
+			<div class="container">
+				<div class="row justify-content-center">
+					<p class="hero__sub-title">How To Give Back</p>
+				</div>	
+				<div class="row justify-content-center">
+					<h1 class="hero__title">Donate</h1>
+				</div>	
+				<div class="row justify-content-center">
+					<p class="hero__hummingbird">Our students thrive because of people like you. Your generous donations help them achieve their goals and learn the skill to live a successful life.</p>
+				</div>	
+			</div>
+		</section>
+
